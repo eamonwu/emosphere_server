cbb03342fe9cbc9d6e8065a00e92314e4a280ef7
eamonwu <eamonwupg@gmail.com>
2023-11-07 18:00:52 +0800

    init

---
+++ src/main/java/com/emosphere/emosphere/EmosphereApplication.java
@@ -0,0 +1,13 @@
+package com.emosphere.emosphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmosphereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmosphereApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emosphere/emosphere/EmosphereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emosphere.emosphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmosphereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
